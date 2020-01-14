3a1b50241f512504db0ce83ed3d525a70b29acba
Timofey Bobkov <bobkov.tim@gmail.com>
2020-01-14 10:29:02 +0300

    fix

--- build_image.sh
+++ build_image.sh
@@ -7,7 +7,7 @@ function getTag() {
     local __tag=$1
     local __defaultVal=$2
     local __resultvar=$3
-    
+
     if [ $__tag = "development" ]; then
         __newValue="Dev"
     elif [ $__tag = "master" ]; then
@@ -27,15 +27,10 @@ fi
 
 if [ ! -z "$2" ]; then
     tagname=$2
-else 
-
+else
     branch=$(git branch | grep \* | cut -d ' ' -f2-)
-
-
     getTag $quorum_version 2.0.2 quorum_version
-
     getTag $branch $quorum_maker_version quorum_maker_version
-
     tagname=$quorum_version"_"$quorum_maker_version
 fi
 
@@ -43,9 +38,9 @@ dockername=$dockerImage":"$tagname
 echo $CYAN"Building image, "$dockername"..."$COLOR_END
 
 lib/install_quorum.sh
-lib/install_tessera.sh
 lib/build_nodemanager.sh
 lib/build_ui.sh
+lib/install_tessera.sh
 
 
 docker build --no-cache -t $dockername .
