46c49d7c97274e86a8cf73ea9f99ddbe107d4995
Timofey Bobkov <bobkov.tim@gmail.com>
2020-01-22 10:10:24 +0300

    update baseimage script

--- lib/install_baseimage.sh
+++ lib/install_baseimage.sh
@@ -2,18 +2,26 @@
 
 #This is for base tag of image syneblock/quorum-master:base
 apt-get update
-apt-get install -y libdb-dev libleveldb-dev libsodium-dev zlib1g-dev libtinfo-dev
-apt-get install -y build-essential
-apt-get install software-properties-common python-software-properties -y
-apt-get install -y wget
-apt-get install -y curl
-apt-get install -y jq
-apt-get install -y psmisc
-apt-get install -y iputils-ping
+apt-get install -y \
+    libdb-dev \
+    libleveldb-dev \
+    libsodium-dev \
+    zlib1g-dev \
+    libtinfo-dev \
+    software-properties-common \
+    python-software-properties \
+    build-essential \
+    wget \
+    curl \
+    jq \
+    psmisc \
+    iputils-ping \
+    bsdmainutils \
+    openjdk-8-jdk \
+    maven \
+    net-tools
 add-apt-repository ppa:ethereum/ethereum -y
 apt-get update
-apt-get install solc -y
-apt-get install bsdmainutils
-apt-get install -y openjdk-8-jdk
-apt-get -y install maven
-apt-get -y install net-tools
+apt-get install -y solc
+apt-get clean
+rm -rf /var/lib/apt/lists/*
